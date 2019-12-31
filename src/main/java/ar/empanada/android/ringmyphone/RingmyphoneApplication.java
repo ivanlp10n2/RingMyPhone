4ded3b19c62566030aa3b289c1745162d685a174
Capitan Empanada <ivanlp10n2@gmail.com>
2019-12-31 07:29:36 -0300

    springboot dependencies

---
+++ src/main/java/ar/empanada/android/ringmyphone/RingmyphoneApplication.java
@@ -0,0 +1,13 @@
+package ar.empanada.android.ringmyphone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RingmyphoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RingmyphoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/empanada/android/ringmyphone/RingmyphoneApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.empanada.android.ringmyphone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RingmyphoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
